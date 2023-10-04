a093a5e7b124794c9edfd3c5c4567ef6d0aa6cce
KananKarim <kenankerimli7770@gmail.com>
2023-10-04 20:07:01 +0400

    getbalance modified

--- typescript-hw/src/main.ts
+++ typescript-hw/src/main.ts
@@ -1,49 +1,57 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 export enum CurrencyEnum {
-    USD = 'USD',
-    UAH = 'UAH'
+  USD = "USD",
+  UAH = "UAH",
 }
 
 export class Transaction {
-    Id: string;
-    Amount: number;
-    Currency: CurrencyEnum;
+  Id: string;
+  Amount: number;
+  Currency: CurrencyEnum;
 
-    constructor(amount: number, currency: CurrencyEnum) {
-        this.Id = uuidv4();
-        this.Amount = amount;
-        this.Currency = currency;
-    }
+  constructor(amount: number, currency: CurrencyEnum) {
+    this.Id = uuidv4();
+    this.Amount = amount;
+    this.Currency = currency;
+  }
 }
 
 export class Card {
-    Transactions: Transaction[];
+  Transactions: Transaction[];
 
-    constructor() {
-        this.Transactions = [];
+  constructor() {
+    this.Transactions = [];
+  }
+
+  AddTransaction(transaction: Transaction): string;
+  AddTransaction(amount: number, currency: CurrencyEnum): string;
+  AddTransaction(
+    amountOrTransaction: number | Transaction,
+    currency?: CurrencyEnum
+  ): string {
+    let transaction: Transaction;
+
+    if (typeof amountOrTransaction === "number") {
+      transaction = new Transaction(amountOrTransaction, currency!);
+    } else {
+      transaction = amountOrTransaction;
     }
 
-    AddTransaction(transaction: Transaction): string;
-    AddTransaction(amount: number, currency: CurrencyEnum): string;
-    AddTransaction(amountOrTransaction: number | Transaction, currency?: CurrencyEnum): string {
-        let transaction: Transaction;
+    this.Transactions.push(transaction);
+    return transaction.Id;
+  }
 
-        if (typeof amountOrTransaction === 'number') {
-            transaction = new Transaction(amountOrTransaction, currency!);
-        } else {
-            transaction = amountOrTransaction;
-        }
+  GetTransaction(id: string): Transaction | undefined {
+    return this.Transactions.find((t) => t.Id === id);
+  }
 
-        this.Transactions.push(transaction);
-        return transaction.Id;
-    }
-
-    GetTransaction(id: string): Transaction | undefined {
-        return this.Transactions.find(t => t.Id === id);
-    }
-
-    GetBalance(currency: CurrencyEnum): number {
-        return this.Transactions.filter(t => t.Currency === currency).reduce((sum, t) => sum + t.Amount, 0);
-    }
+  GetBalance(currency: CurrencyEnum): number {
+    return this.Transactions.reduce((sum, t) => {
+      if (t.Currency === currency) {
+        return sum + t.Amount;
+      }
+      return sum;
+    }, 0);
+  }
 }
