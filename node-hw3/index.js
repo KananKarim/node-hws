7e26e0b74cdcb7cc90ae9448135d629ef63dd1fd
KananKarim <kenankerimli7770@gmail.com>
2023-10-04 20:56:20 +0400

    change condition

--- node-hw3/index.js
+++ node-hw3/index.js
@@ -30,8 +30,8 @@ app.get('/', (req, res) => {
     }
 });
 
-const host = process.env.HOST || '127.0.0.1';
-const port = process.env.PORT || 3000;
+const host = process.env.HOST
+const port = process.env.PORT
 
 app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
